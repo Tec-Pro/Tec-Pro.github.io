02131a97e8980ea1c71c29aeda75634945d30e60
jjaimez <jaimezjacinto@gmail.com>
2015-02-07 19:07:41 -0200

    prototype

---
+++ js/funcionalidades.js
@@ -0,0 +1,23 @@
+<!--
+
+function controlar(){
+            if ((document.getElementById('name').value == '') || (document.getElementById('name').value == ' ')){
+                alert('Ingrese su nombre por favor');
+            } else {
+                if ((document.getElementById('email').value == '') || (document.getElementById('email').value == ' ')){
+                    alert('Ingrese su email por favor');
+                } else {
+                    if ((document.getElementById('message').value == '') || (document.getElementById('message').value == ' ')){
+                        alert('Ingrese el asunto por favor');
+                    } else {
+                         if ((document.getElementById('message').value == '') || (document.getElementById('message').value == ' ')){
+                            alert('Ingrese el mensaje por favor');
+                        } else{
+                            document.getElementById('form-contacto').submit();
+                        }
+                    }
+                }               
+            }
+        }
+
+-->
